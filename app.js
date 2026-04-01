07b1a152012a6c3d29669c7d6270d2233fbb58b3
Gianfranco <gianfranco.devita@hotmail.com>
2026-04-01 11:26:32 +0200

    fix: fixed put and delete routes

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const logger = require("morgan")
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 
-const Pizza = require("./models/Pizza.model")
+const Pizza = require("./models/Pizza.model");
 
-
-const app = express()
-
+const app = express();
 
 // Make the static files inside of the `public/` folder publicly accessible
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // Setup the request logger to run on each request
-app.use(logger("dev"))
+app.use(logger("dev"));
 
 // JSON middleware to parse incoming HTTP requests that contain JSON
 app.use(express.json());
-
 
 /*********************/
 /* Connect to the DB */
 /*********************/
 
-mongoose.connect("mongodb://127.0.0.1:27017/express-restaurant")
-    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-    .catch(err => console.error("Error connecting to mongo", err));
-
-
+mongoose
+	.connect("mongodb://127.0.0.1:27017/express-restaurant")
+	.then((x) =>
+		console.log(
+			`Connected to Mongo! Database name: "${x.connections[0].name}"`,
+		),
+	)
+	.catch((err) => console.error("Error connecting to mongo", err));
 
 /**********/
 /* ROUTES */
@@ -35,113 +35,104 @@ mongoose.connect("mongodb://127.0.0.1:27017/express-restaurant")
 // app.get(path, code)
 // app.get(path, (req, res, next) => {})
 
-
 // GET /
 app.get("/", (req, res, next) => {
-    console.log("we received a GET request for the HOME page...");
-    // res.send("")    
-    res.sendFile(__dirname + "/views/home.html")
-})
+	console.log("we received a GET request for the HOME page...");
+	// res.send("")
+	res.sendFile(__dirname + "/views/home.html");
+});
 
 // GET /contact
 app.get("/contact", (req, res, next) => {
-    console.log("we received a GET request for the CONTACT page...");
-    // res.send("")
-    res.sendFile(__dirname + "/views/contact.html")
-})
-
+	console.log("we received a GET request for the CONTACT page...");
+	// res.send("")
+	res.sendFile(__dirname + "/views/contact.html");
+});
 
 // POST /pizzas -- Create a new pizza
 app.post("/pizzas", (req, res, next) => {
-    
-    const newPizza = req.body
+	const newPizza = req.body;
 
-    Pizza.create(newPizza)
-        .then((pizzaFromDB) => {
-            res.status(201).json(pizzaFromDB)
-        })
-        .catch((err) => {
-            console.log("error creating a new pizza...\n\n", err)
-            res.status(500).json({ error: "Error creating a new pizza in the DB..." })
-        })
-})
-
-
+	Pizza.create(newPizza)
+		.then((pizzaFromDB) => {
+			res.status(201).json(pizzaFromDB);
+		})
+		.catch((err) => {
+			console.log("error creating a new pizza...\n\n", err);
+			res
+				.status(500)
+				.json({ error: "Error creating a new pizza in the DB..." });
+		});
+});
 
 // GET /pizzas -- Get the list of pizzas
 // GET /pizzas?maxPrice=16 -- all pizzas with a max price of 16
 app.get("/pizzas", (req, res, next) => {
+	let { maxPrice } = req.query;
 
-    let { maxPrice } = req.query
+	let filter = {};
 
-    let filter = {}
+	if (maxPrice !== undefined) {
+		filter = { price: { $lte: maxPrice } };
+	}
 
-    if (maxPrice !== undefined) {
-        filter = { price: { $lte: maxPrice } }
-    }
-
-    Pizza.find(filter)
-        .then((pizzasFromDB) => {
-            res.json(pizzasFromDB)
-        })
-        .catch((err) => {
-            console.log("Error getting pizzas from DB... \n\n", err)
-            res.status(500).json({ error: "Failed to get list of pizzas" })
-        })
-})
-
+	Pizza.find(filter)
+		.then((pizzasFromDB) => {
+			res.json(pizzasFromDB);
+		})
+		.catch((err) => {
+			console.log("Error getting pizzas from DB... \n\n", err);
+			res.status(500).json({ error: "Failed to get list of pizzas" });
+		});
+});
 
 // GET /pizzas/:pizzaId -- Get the details for one pizza
 app.get("/pizzas/:pizzaId", (req, res, next) => {
+	const { pizzaId } = req.params;
 
-    const { pizzaId } = req.params
-
-    Pizza.findById(pizzaId)
-        .then((pizzaFromDB) => {
-            res.json(pizzaFromDB)
-        })
-        .catch(err => {
-            console.log("Error getting pizza details from DB...\n\n", err);
-            res.status(500).json({ error: "Failed to get pizza details" });
-        })
-})
-
-
+	Pizza.findById(pizzaId)
+		.then((pizzaFromDB) => {
+			res.json(pizzaFromDB);
+		})
+		.catch((err) => {
+			console.log("Error getting pizza details from DB...\n\n", err);
+			res.status(500).json({ error: "Failed to get pizza details" });
+		});
+});
 
 // PUT /pizzas/:pizzaId -- Update one pizza
 app.put("/pizzas/:pizzaId", function (req, res, next) {
+	const { pizzaId } = req.params;
+	const newDetails = req.body;
 
-    const { pizzaId } = req.params
-
-    Pizza.findByIdAndUpdate(pizzaId, newDetails, { new: true })
-        .then((pizzaFromDB) => {
-            res.json(pizzaFromDB)
-        })
-        .catch((err) => {
-            console.error("Error updating pizza...");
-            res.json({ error: "Failed to update a pizza" });
-        })
-})
-
-
+	Pizza.findByIdAndUpdate(pizzaId, newDetails, { new: true })
+		.then((pizzaFromDB) => {
+			res.json(pizzaFromDB);
+		})
+		.catch((err) => {
+			console.error("Error updating pizza...", err);
+			res.status(500).json({ error: "Failed to update a pizza" });
+		});
+});
 
 // DELETE /pizzas/:pizzaId -- Delete one pizza
 app.delete("/pizzas/:pizzaId", (req, res, next) => {
-    Pizza.findByIdAndDelete()
-        .then((response) => {
-            res.json(response)
-        })
-        .catch((err) => {
-            console.error("Error deleting pizza...");
-            res.json({ error: err });
-        })
-})
+	const { pizzaId } = req.params;
 
-
-
+	Pizza.findByIdAndDelete(pizzaId)
+		.then((response) => {
+			res.json(response);
+		})
+		.catch((err) => {
+			console.error("Error deleting pizza...", err);
+			res.status(500).json({ error: err });
+		});
+});
 
 /****************/
 /* START SERVER */
 /****************/
 
-app.listen(3000, () => { console.log("Server listening on port 3000....") })
+app.listen(3000, () => {
+	console.log("Server listening on port 3000....");
+});
